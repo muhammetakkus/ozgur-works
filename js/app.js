314f213cbd5ebc469429a03a8a18aa5f0ca25a8f
Mehmet Akkuş <muhammetakkuss34@gmail.com>
2024-07-21 15:53:01 +0300

    git works

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import Menu from "./menu"
+
+const test = new Menu;
+
+console.log(test.isResponsive())
---
+++ js/menu.js
@@ -0,0 +1,8 @@
+class Menu {
+    /* METHOD */ 
+    isResponsive() {
+        console.log('Responsive Menu test');
+    }
+}
+
+export default Menu
